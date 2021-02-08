30d2cf5b510a6bda1f3c782c1f62721b6dee5347
filiphric <filip@slido.com>
2021-02-09 00:17:01 +0100

    add some tests

---
+++ cypress/integration/failed.ts
@@ -0,0 +1,27 @@
+beforeEach(() => {
+
+  cy
+    .request('POST', '/reset');
+
+});
+
+it('opens board', () => {
+
+  cy
+    .visit('/');
+
+  cy
+    .log('click on create a board button')
+    .get('[data-cy="create-board"]')
+    .click();
+
+  cy
+    .log('type in board name and hit enter')
+    .get('[data-cy="new-board-input"]')
+    .type('new board{enter}');
+
+  cy
+    .url()
+    .should('eq', 'nonsens');
+
+});
---
+++ cypress/integration/flaky.ts
@@ -0,0 +1,26 @@
+it('board id must be even', () => {
+
+  cy
+    .intercept('POST', '/api/boards')
+    .as('board');
+
+  cy
+    .visit('/');
+
+  cy
+    .get('[data-cy="create-board"]')
+    .click();
+
+  cy
+    .get('[data-cy=new-board-input]')
+    .type('new board{enter}');
+
+  cy
+    .wait('@board')
+    .then(({ response }) => {
+
+      expect(response.body.id % 2 == 0).to.be.true;
+
+    });
+
+});
